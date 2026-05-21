33521cb30d16b1d1ddba840617641eba1de3a81f
Tracy Lethoko <tracylethoko@Tracys-MacBook-Pro.local>
2026-05-21 17:17:39 +0200

    updated min to use new structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { podcasts } from "./data.js";
 import { createModal } from "./components/createModal.js";
 import { createGrid } from "./views/createGrid.js";
+import "./components/PodcastPreview.js";
+
 
 /**
  * Initializes the podcast application.
@@ -11,8 +13,19 @@ function init() {
   document
     .getElementById("closeModal")
     .addEventListener("click", createModal.close);
-  const grid = createGrid();
-  grid.render(podcasts);
-}
 
-init();
+    const clearPlaceholder = document.getElementById("podcastGrid");
+    clearPlaceholder.innerHTML = "";
+
+    const gridLayout = createGrid(podcasts);
+    clearPlaceholder.appendChild(gridLayout);
+
+    clearPlaceholder.addEventListener("podcast-selected", (event) => {
+      // Extract the data sent from the click
+      const podcastData = event.detail;
+
+      createModal.open(podcastData);
+    });
+  }
+
+  init();
